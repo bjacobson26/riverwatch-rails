bb19093436aae752613f8155f1ba2a0b564e2b74
Bo Jacobson <bo@software.com>
2023-01-09 22:48:37 -0800

    turboframes

--- app/javascript/controllers/autoreload_controller.js
+++ app/javascript/controllers/autoreload_controller.js
@@ -4,17 +4,11 @@ export default class extends Controller {
   connect() {
     // reload turboframes every 60 seconds
     document.querySelectorAll('turbo-frame').forEach((turboframe) => {
-      let interval = 60000
-      if(turboframe.id == "highway") {
-        interval = 65000
-      }
-
-      let src = turboframe.src
-
       setInterval(() => {
+        let src = turboframe.src
         turboframe.src = null
         turboframe.src = src
-      }, interval)
+      }, 60000)
     })
   }
 }
